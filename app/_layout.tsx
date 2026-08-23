dd47344a318813e943985271ea8d47c38cc44aee
Hari Hans Hansah <hharihans@gmail.com>
2026-08-23 23:34:48 +0530

    gtag

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -138,6 +138,17 @@ export default function RootLayout() {
   return (
     <ThemeProvider>
       <Head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9NCRSRJ8ZV"></script>
+<script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9NCRSRJ8ZV');
+            `,
+          }}
+        />
         <title>AutoSaathi JSR</title>
         <link 
           rel="icon" 
